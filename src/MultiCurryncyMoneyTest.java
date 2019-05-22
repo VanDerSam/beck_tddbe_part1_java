63d8b7f473eeb503956b4fad18b8edb389b3f9fc
Sergey Muratov <van.der.msa@gmail.com>
2019-05-22 09:15:22 +0300

    State after Chapter 5 (bug fixing)

--- src/MultiCurryncyMoneyTest.java
+++ src/MultiCurryncyMoneyTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 public class MultiCurryncyMoneyTest {
 	@Test
 	public void testMultiplication() {
-		Franc five = new Franc(5);
-		assertEquals(new Franc(10), five.times(2));
-		assertEquals(new Franc(15), five.times(3));
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
 	}
 	
 	@Test
 	public void testEquality() {
-		assertTrue(new Franc(5).equals(new Franc(5)));
-		assertFalse(new Franc(5).equals(new Franc(6)));
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 	
 	@Test
