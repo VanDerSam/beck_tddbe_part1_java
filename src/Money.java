c2d93732d091c01cc6ad19a9d4000fdda45b3f38
Sergey Muratov <van.der.msa@gmail.com>
2019-05-22 10:07:09 +0300

    State after Chapter 9

--- src/Money.java
+++ src/Money.java
@@ -1,6 +1,12 @@
 
 abstract public class Money {
 	protected int amount;
+	protected String currency;
+	
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 	
 	@Override
 	public boolean equals(Object object) {
@@ -10,12 +16,16 @@ abstract public class Money {
 	}
 	
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 	
 	abstract Money times(int multiplier);
 	
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
+	}
+	
+	String currency() {
+		return currency;
 	}
 }
